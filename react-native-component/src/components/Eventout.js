1057f8984fd45c0f19c83405b798659e02f3c8cd
zlzkzlgh <zlzkzlgh@gmail.com>
2024-10-29 17:31:39 +0900

    rn

---
+++ react-native-component/src/components/Eventout.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { View,Text,TextInput } from "react-native";
+
+const EventInput = () => {
+
+    //text를 저장할 state
+    const [text,setText] = useState('');
+
+    const _onChange = event => (event.nativeEvent.text);
+
+    return(
+        <View>
+            <Text style={{margin:10,fontSize:30}}></Text>
+            <TextInput
+                style={{borderWidth:1,padding:10,fontSize:20}}
+                placeholder='Enter the Text'
+                onChange={_onChange}/>
+        </View>
+    )
+}
